a5ec40231956fa97485ab19e340ee4d8832da511
Jitesh <85620326+voidstarsh@users.noreply.github.com>
2021-06-14 22:59:58 +0530

    Remove hardcoded dmenu integration

--- dwm.c
+++ dwm.c
@@ -1668,8 +1668,6 @@ sigchld(int unused)
 void
 spawn(const Arg *arg)
 {
-	if (arg->v == dmenucmd)
-		dmenumon[0] = '0' + selmon->num;
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));

732a35c5b76d975250746b3ab18d8dfca16bd006
Jitesh <85620326+voidstarsh@users.noreply.github.com>
2021-08-19 00:21:42 +0530

    Remove -Wunused-function warnings

--- patch/bar_wintitle.h
+++ patch/bar_wintitle.h
@@ -1,3 +1,3 @@
-static int width_wintitle(Bar *bar, BarWidthArg *a);
-static int draw_wintitle(Bar *bar, BarDrawArg *a);
-static int click_wintitle(Bar *bar, Arg *arg, BarClickArg *a);
+int width_wintitle(Bar *bar, BarWidthArg *a);
+int draw_wintitle(Bar *bar, BarDrawArg *a);
+int click_wintitle(Bar *bar, Arg *arg, BarClickArg *a);

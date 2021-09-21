712842ae0d5a6a9e2bb2691a1eb3345cf0924b46
Jitesh <85620326+voidstarsh@users.noreply.github.com>
2021-09-21 21:40:09 +0530

    Organize config.h (prev 41bea46)

--- config.h
+++ config.h
@@ -1,11 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 
-/* -------------- appearance ------------- */
+/* --------------- settings -------------- */
 static const char *fonts[]      = { "JetBrainsMono Nerd Font Mono:style=medium:size=10", "Material Icons:size=10" };
 static unsigned int borderpx    = 2;        /* border pixel of windows */
 static unsigned int snap        = 10;       /* snap pixel */
 static int swallowfloating      = 0;        /* 1 means swallow floating windows by default */
+static const float mfact        = 0.50;     /* factor of master area size [0.05..0.95] */
+static const int nmaster        = 1;        /* number of clients in master area */
+static const int resizehints    = 0;        /* 1 means respect size hints in tiled resizals */
 
+/* ----------------- gaps ---------------- */
 static          int smartgaps   = 0;        /* 1 means no outer gap when there is only one window */
 static          int enablegaps  = 1;        /* 1 means enable gaps on startup */
 static unsigned int gappih      = 10;       /* horiz inner gap between windows */
@@ -13,6 +17,7 @@ static unsigned int gappiv      = 10;       /* vert inner gap between windows */
 static unsigned int gappoh      = 10;       /* horiz outer gap between windows and screen edge */
 static unsigned int gappov      = 10;       /* vert outer gap between windows and screen edge */
 
+/* ----------------- bar ----------------- */
 #define BARHEIGHT_STR           "27"        /* dmenu_run's -h value, match with barheight below */
 static int barheight            = 27;       /* 0 means dwm will calculate bar height wrt font */
 static int showbar              = 1;        /* 0 means no bar */
@@ -27,37 +32,38 @@ static char *colors[][3]        = {
 	[SchemeUrg]  = { color9,        background,     color9  },
 };
 
-/* ----------------- patches ------------- */
+/* ----------------- tags ---------------- */
+static const char *tags[]       = { "", "", "", "", "", "", "" };
+
+/* --------------- patches --------------- */
+#define FORCE_VSPLIT            1           /* nrowgrid layout: force two clients to always split vertically */
 #define VIEWONTAG               1           /* switch view on tag switch */
 #define PERTAG_VANITYGAPS       1           /* vanitygaps per tag */
 
-/* ---------------- systray -------------- */
+/* --------------- systray --------------- */
 static unsigned int systrayspacing  = 3;    /* systray spacing */
 static          int showsystray     = 1;    /* 0 means no systray */
 
-/* --------------- autostart ------------- */
+/* -------------- autostart -------------- */
 static const char autostartsh[]     = "autostart.sh";
 static const char autostartwaitsh[] = "autostartwait.sh";
 static const char dwmdir[]          = "dwm";     /* parent dir of autostart(wait).sh */
 static const char pathfromhome[]    = ".config"; /* custom dir from $HOME if $XDG_CONFIG_HOME was not set; don't add leading & trailing '/' */
 
-/* ------------- occ indicator ----------- */
+/* ------------ occ indicator ------------ */
 static const unsigned int ulinepad      = 5;     /* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke   = 2;     /* underline thickness */
 static const unsigned int ulinevoffset  = 0;     /* how far above the bottom of the bar the line should appear */
 
-/* ----------------- tags ---------------- */
-static const char *tags[] = { "", "", "", "", "", "", "" };
-
 /* --------------- defaults -------------- */
-#define APP_BROWSER     "firefox"
-#define APP_BROWSER_    "firefox --private-window"
-#define APP_MENU        "dmenu_run"
-#define APP_TERMINAL    "st"
-#define CLASS_TERMINAL  "St"
+#define APP_BROWSER             "firefox"
+#define APP_BROWSER_            "firefox --private-window"
+#define APP_MENU                "dmenu_run"
+#define APP_TERMINAL            "st"
+#define CLASS_TERMINAL          "St"
 
-/* ----------------- rules --------------- */
-static const Rule rules[] = {
+/* ---------------- rules ---------------- */
+static const Rule rules[]       = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
@@ -69,7 +75,7 @@ static const Rule rules[] = {
 	{ NULL,             NULL,       "Event Tester", 0,          0,          0,          1,          -1,     0   },
 };
 
-/* ------------- barmodules -------------- */
+/* -------------- barmodules ------------- */
 static const BarRule barrules[] = {
 	/* Bar rules allow you to configure what is shown where on the bar, as well as
 	 * introducing your own bar modules.
@@ -92,58 +98,52 @@ static const BarRule barrules[] = {
 	/* { 'A',      0,     BAR_ALIGN_RIGHT_RIGHT,   width_status,          draw_status,          click_status,       "status"   }, */
 };
 
-static const float mfact     = 0.50;        /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;           /* number of clients in master area */
-static const int resizehints = 0;           /* 1 means respect size hints in tiled resizals */
-
-#define FORCE_VSPLIT 1                      /* nrowgrid layout: force two clients to always split vertically */
+/* --------------- layouts --------------- */
 #include "vanitygaps.c"
-
-/* ---------------- layouts -------------- */
-static const Layout layouts[] = {
-    /* symbol   arrange function */
-    { "",    tile },                       /* Default: Master on left, Slaves on right */
-    { "",    bstack },                     /* Master on top, Slaves on bottom */
-    { "",    grid },                       /* Grid layout */
-    { "",    gaplessgrid },                /* Grid layout but without gaps */
-    { "",    centeredmaster },             /* Master in middle, Slaves on sides */
-    { "",    centeredfloatingmaster },     /* Same as centeredmaster but Master floats */
-    { "",    monocle },                    /* All windows on top of each other */
-    { "",    deck },                       /* Master on left, Slaves in monocle-like mode on right */
-    { "",    spiral },                     /* Fibonacci spiral */
-    { "",    dwindle },                    /* Decreasing in size right and leftward */
-    { "",    NULL },                       /* No layout function means floating behaviour */
-    { NULL,     NULL },
+static const Layout layouts[]   = {
+	/* symbol   arrange function */
+	{ "",      tile },                     /* Default: Master on left, Slaves on right */
+	{ "",      bstack },                   /* Master on top, Slaves on bottom */
+	{ "",      grid },                     /* Grid layout */
+	{ "",      gaplessgrid },              /* Grid layout but without gaps */
+	{ "",      centeredmaster },           /* Master in middle, Slaves on sides */
+	{ "",      centeredfloatingmaster },   /* Same as centeredmaster but Master floats */
+	{ "",      monocle },                  /* All windows on top of each other */
+	{ "",      deck },                     /* Master on left, Slaves in monocle-like mode on right */
+	{ "",      spiral },                   /* Fibonacci spiral */
+	{ "",      dwindle },                  /* Decreasing in size right and leftward */
+	{ "",      NULL },                     /* No layout function means floating behaviour */
+	{ NULL,     NULL },
 };
 
-/* --------------- Xresources ------------ */
-ResourcePref resources[] = {
-		{ "foreground", STRING,  &foreground },
-		{ "background", STRING,  &background },
-		{ "color0",     STRING,  &color0 },
-		{ "color8",     STRING,  &color8 },
-		{ "color1",     STRING,  &color1 },
-		{ "color9",     STRING,  &color9 },
-		{ "color2",     STRING,  &color2 },
-		{ "color10",    STRING,  &color10 },
-		{ "color3",     STRING,  &color3 },
-		{ "color11",    STRING,  &color11 },
-		{ "color4",     STRING,  &color4 },
-		{ "color12",    STRING,  &color12 },
-		{ "color5",     STRING,  &color5 },
-		{ "color13",    STRING,  &color13 },
-		{ "color6",     STRING,  &color6 },
-		{ "color14",    STRING,  &color14 },
-		{ "color7",     STRING,  &color7 },
-		{ "color15",    STRING,  &color15 },
-		{ "barheight",  INTEGER, &barheight },
-		{ "showbar",    INTEGER, &showbar },
-		{ "topbar",     INTEGER, &topbar },
-		{ "borderpx",   INTEGER, &borderpx },
-		{ "snap",       INTEGER, &snap },
-		{ "enablegaps", INTEGER, &enablegaps },
-		{ "smartgaps",  INTEGER, &smartgaps },
-		{ "showsystray",INTEGER, &showsystray },
+/* -------------- Xresources ------------- */
+ResourcePref resources[]        = {
+	{ "foreground", STRING,  &foreground },
+	{ "background", STRING,  &background },
+	{ "color0",     STRING,  &color0 },
+	{ "color8",     STRING,  &color8 },
+	{ "color1",     STRING,  &color1 },
+	{ "color9",     STRING,  &color9 },
+	{ "color2",     STRING,  &color2 },
+	{ "color10",    STRING,  &color10 },
+	{ "color3",     STRING,  &color3 },
+	{ "color11",    STRING,  &color11 },
+	{ "color4",     STRING,  &color4 },
+	{ "color12",    STRING,  &color12 },
+	{ "color5",     STRING,  &color5 },
+	{ "color13",    STRING,  &color13 },
+	{ "color6",     STRING,  &color6 },
+	{ "color14",    STRING,  &color14 },
+	{ "color7",     STRING,  &color7 },
+	{ "color15",    STRING,  &color15 },
+	{ "barheight",  INTEGER, &barheight },
+	{ "showbar",    INTEGER, &showbar },
+	{ "topbar",     INTEGER, &topbar },
+	{ "borderpx",   INTEGER, &borderpx },
+	{ "snap",       INTEGER, &snap },
+	{ "enablegaps", INTEGER, &enablegaps },
+	{ "smartgaps",  INTEGER, &smartgaps },
+	{ "showsystray",INTEGER, &showsystray },
 };
 
 /* ----------- key definitions ----------- */
@@ -161,16 +161,17 @@ ResourcePref resources[] = {
 /* first arg only serves to match against key in rules */
 static const char *scratchpadcmd[] = {"s", "st", "-c", "ScratchPad", "-t", "scratchpad", NULL};
 
-static const char *menucmd[] = { APP_MENU, "-h", BARHEIGHT_STR, NULL };
-static const char *termcmd[] = { APP_TERMINAL, NULL };
+static const char *menucmd[]    = { APP_MENU, "-h", BARHEIGHT_STR, NULL };
+static const char *termcmd[]    = { APP_TERMINAL, NULL };
 
 #include <X11/XF86keysym.h>
-static const char vol_up[]    =  "pactl set-sink-volume 0 +5%; kill -44 $(pidof dwmblocks)" ;
-static const char vol_down[]  =  "pactl set-sink-volume 0 -5%; kill -44 $(pidof dwmblocks)" ;
-static const char vol_mute[]  =  "pactl set-sink-mute 0 toggle; kill -44 $(pidof dwmblocks)";
-#define XCLIP_PNG                " | xclip -selection clipboard -target image/png"
+static const char vol_up[]      =  "pactl set-sink-volume 0 +5%; kill -44 $(pidof dwmblocks)" ;
+static const char vol_down[]    =  "pactl set-sink-volume 0 -5%; kill -44 $(pidof dwmblocks)" ;
+static const char vol_mute[]    =  "pactl set-sink-mute 0 toggle; kill -44 $(pidof dwmblocks)";
+#define XCLIP_PNG               " | xclip -selection clipboard -target image/png"
 
-static Key keys[] = {
+/* ------------- keybindings ------------- */
+static Key keys[]               = {
 	/* modifier                     key         function        argument */
 	{ MODKEY,                       XK_Return,  spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_d,       spawn,          {.v = menucmd } },
@@ -187,8 +188,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Tab,     cyclelayouts,   { .i = -1 } },
 
 	/* ------------ gaps ----------- */
-	{ MODKEY,                       XK_g,       togglegaps,     {0} },        /* toggle gaps */
-	{ MODKEY|ShiftMask,             XK_g,       defaultgaps,    {0} },        /* reset gaps */
+	{ MODKEY,                       XK_g,       togglegaps,     {0} },        /* toggle  gaps */
+	{ MODKEY|ShiftMask,             XK_g,       defaultgaps,    {0} },        /* reset   gaps */
 	{ MODKEY,                       XK_minus,   incrogaps,      {.i = -3 } }, /* dec out gaps */
 	{ MODKEY,                       XK_equal,   incrogaps,      {.i = +3 } }, /* inc out gaps */
 	{ MODKEY|ShiftMask,             XK_minus,   incrigaps,      {.i = -3 } }, /* dec inr gaps */
@@ -232,7 +233,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_r,       reorganizetags, {0} },
 	{ MODKEY|ShiftMask,             XK_r,       distributetags, {0} },
 
-	/* -------- scratchpad --------- */
+	/* --------- scratchpad -------- */
 	{ MODKEY,                       XK_n,       setscratch,     {.v = scratchpadcmd } },
 	{ MODKEY|ShiftMask,             XK_n,       removescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_p,       togglescratch,  {.v = scratchpadcmd } },
@@ -271,9 +272,9 @@ static Key keys[] = {
 	/* TAGKEYS(                        XK_9,                      8) */
 };
 
-/* button definitions */
+/* ---------- button definitions --------- */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static Button buttons[] = {
+static Button buttons[]         = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
@@ -281,7 +282,7 @@ static Button buttons[] = {
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	/* placemouse options, choose which feels more natural:
 	 *    0 - tiled position is relative to mouse cursor
-	 *    1 - tiled postiion is relative to window center
+	 *    1 - tiled position is relative to window center
 	 *    2 - mouse pointer warps to window center
 	 *
 	 * The moveorplace uses movemouse or placemouse depending on the floating state
